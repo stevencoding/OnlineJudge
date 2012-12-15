7e63ff1c8c45ddf2e30f0233a87736e921b32c83
Daisy Gao <aringao@gmail.com>
2012-12-15 16:49:32 +0800

    Count and Say

--- leetcode/countAndSay.java
+++ leetcode/countAndSay.java
@@ -1,48 +1,25 @@
-public class Solution {    public String countAndSay(int n) {
-
-    // Start typing your Java solution below
-
-    // DO NOT write main() function
-
-    String result = "";
-
-    if (n == 0) return result;
-
-    result = "1";
-
-    String buffer;
-
-    while (--n != 0) {
-
-	buffer = "";
-
-	int len = result.length();
-
-	for (int i = 0; i < len; i++) {
-
-	    int count = 1;
-
-	    while (i + 1 < len && result.charAt(i + 1) == result.charAt(i)) {
-
-		count++;
-
-		i++;
-
+public class Solution {   
+    public String countAndSay(int n) {
+	// Start typing your Java solution below
+	// DO NOT write main() function
+	String result = "";
+	if (n == 0) return result;
+	result = "1";
+	String buffer;
+	while (--n != 0) {
+	    buffer = "";
+	    int len = result.length();
+	    for (int i = 0; i < len; i++) {
+		int count = 1;
+		while (i + 1 < len && result.charAt(i + 1) == result.charAt(i)) {
+		    count++;
+		    i++;
+		}
+		buffer += Integer.toString(count) + result.charAt(i);
 	    }
-
-	    buffer += Integer.toString(count) + result.charAt(i);
-
+	    result = buffer;
 	}
-
-	result = buffer;
-
+	return result;
     }
-
-    return result;
-
-       
-
-}
-
 }
 
